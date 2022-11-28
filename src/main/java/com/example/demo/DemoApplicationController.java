084a6331f77e4933e8e77ac41a3f5b624f15cd45
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2022-11-28 20:42:32 -0300

    subindo projeto

---
+++ src/main/java/com/example/demo/DemoApplicationController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoApplicationController {
+	@GetMapping("/listar")
+	
+	public String metodo(){
+		return "api ok!";
+	}
+
+}
