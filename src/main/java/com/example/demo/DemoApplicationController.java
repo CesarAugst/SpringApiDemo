53c3807379ae5b69dac1d0443286f3dd98c51c0d
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2022-11-28 20:56:51 -0300

    remoção de dependencia que nao usa

--- src/main/java/com/example/demo/DemoApplicationController.java
+++ src/main/java/com/example/demo/DemoApplicationController.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
